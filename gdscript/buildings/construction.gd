669be4179fbcc1cb5ffaa1aced15c20d607cd5fb
lousky690 <lousky690@gmail.com>
2024-12-11 09:37:37 +0100

    end

---
+++ gdscript/buildings/construction.gd
@@ -0,0 +1,39 @@
+class_name Construction extends Building
+
+var building:Node
+var progress:float
+var difficulty:int = 100 #říká jak moc těžké je postavit budovu ,do toho se počítá i velikost budovy
+var effectivness:int = 5
+var expansion:bool
+
+func _init() -> void:
+	inventory = {"crop": 0, "food":0, "clothes":0, "furniture":0, "electronics":0, "steel":0, "wood":0}
+	level = 1
+
+func _on_tick() -> void:
+	if money > 0:
+		buy_needs(money/2)
+		if !inventory["steel"] >= (1 - progress) * 100 * difficulty:
+			make_demand.emit(self, "steel", ((money / 2) / market.get_commodity("steel").prize))
+		if !inventory["woood"] >= (1 - progress) * 100 * difficulty:
+			make_demand.emit(self, "wood", (money / market.get_commodity("wood").prize))
+	_work()
+	if progress >= 1:
+		if expansion:
+			building.level += 1
+		else:
+			get_parent().add_child(building)
+
+func _work():
+	if (inventory["wood"] < inventory["steel"] and inventory["wood"] == effectivness * difficulty) or (inventory["wood"] > inventory["steel"] and inventory["steel"] == effectivness * difficulty):
+		progress += effectivness / difficulty
+		inventory["wood"] -= effectivness * difficulty
+		inventory["steel"] -= effectivness * difficulty
+	elif inventory["wood"] < inventory["steel"]:
+		progress += inventory["wood"] / difficulty / 100
+		inventory["wood"] = 0
+		inventory["steel"] -= inventory["wood"]
+	elif inventory["wood"] > inventory["steel"]:
+		progress += inventory["steel"] / difficulty / 100
+		inventory["steel"] = 0
+		inventory["wood"] -= inventory["steel"]
---
+++ gdscript/buildings/goverment_construction.gd
@@ -0,0 +1,28 @@
+class_name GovermentConstruction extends Construction
+
+signal get_state_money(me:Node)
+
+func _ready() -> void:
+	country = get_parent().get_parent()
+	market = country.get_parent()
+	
+	make_supply.connect(market._on_make_supply)
+	make_demand.connect(market._on_make_demand)
+	property_tax_pay.connect(country._on_property_tax_pay)
+	get_state_money.connect(country._on_building_demands_money)
+	TimeManager.tick.connect(_on_tick)
+
+func _on_tick() -> void:
+	get_state_money.emit(self)
+	if money > 0:
+		buy_needs(money/2)
+		if !inventory["steel"] >= (1 - progress) * 100 * difficulty:
+			make_demand.emit(self, "steel", ((money / 2) / market.get_commodity("steel").prize))
+		if !inventory["woood"] >= (1 - progress) * 100 * difficulty:
+			make_demand.emit(self, "wood", (money / market.get_commodity("wood").prize))
+	_work()
+	if progress >= 1:
+		if expansion:
+			building.level += 1
+		else:
+			get_parent().add_child(building)
